1cfb1c6ccc0896091ace318e29d594ddd551ffe2
CarlosBueloni <carlosbueloni@gmail.com>
2021-11-08 15:47:57 -0300

    Fix request url on loginuser model

--- todo/views.py
+++ todo/views.py
@@ -32,11 +32,10 @@ def signupuser(request):
 
 def loginuser(request):
     if request.method == 'GET':
-        return render(request, 'todo/signupuser.html', {'form':AuthenticationForm()})
-    
+        return render(request, 'todo/loginuser.html', {'form':AuthenticationForm()})
     user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
     if user is None:
-        return render(request, 'todo/signupuser.html', {'form':AuthenticationForm(), 'error': 'Username password did not match.'})
+        return render(request, 'todo/loginuser.html', {'form':AuthenticationForm(), 'error': 'Username password did not match.'})
     
     login(request, user)
     return redirect('currenttodos')
