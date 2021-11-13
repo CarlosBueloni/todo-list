3c44b8041d7edd6c9ee736602e9bddade39c5639
CarlosBueloni <carlosbueloni@gmail.com>
2021-11-13 11:24:48 -0300

    Refactor names

--- todo/views.py
+++ todo/views.py
@@ -15,38 +15,44 @@ def home(request):
 
 
 
-def signupuser(request):
+def signup_user(request):
     if request.method == 'GET':
-        return render(request, 'todo/signupuser.html', {'form':UserCreationForm()})
+        return render(request, 'todo/signup_user.html', {'form':UserCreationForm()})
     if request.POST['password1'] != request.POST['password2']:
-        return render(request, 'todo/signupuser.html', {'form':UserCreationForm(), 'error':'Passwords did not match'})
+        return render(request, 'todo/signup_user.html', {'form':UserCreationForm(), 'error':'Passwords did not match'})
         
     try:
         user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
         user.save()
         login(request, user)
-        return redirect('currenttodos')
+        return redirect('current_todos')
     except IntegrityError:
-        return render(request, 'todo/signupuser.html', {'form':UserCreationForm(), 'error':'User already exists'})
+        return render(request, 'todo/signup_user.html', {'form':UserCreationForm(), 'error':'User already exists'})
 
 
-def loginuser(request):
+def login_user(request):
     if request.method == 'GET':
-        return render(request, 'todo/loginuser.html', {'form':AuthenticationForm()})
+        return render(request, 'todo/login_user.html', {'form':AuthenticationForm()})
     user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
     if user is None:
-        return render(request, 'todo/loginuser.html', {'form':AuthenticationForm(), 'error': 'Username password did not match.'})
+        return render(request, 'todo/login_user.html', {'form':AuthenticationForm(), 'error': 'Username password did not match.'})
     
     login(request, user)
-    return redirect('currenttodos')
+    return redirect('current_todos')
 
 
 
-def logoutuser(request):
+def logout_user(request):
     if request.method == 'POST':
         logout(request)
         return redirect('home')
 
-def currenttodos(request):
-    return(render(request, 'todo/currenttodos.html'))
+def create_todo(request):
+    if request.method == 'GET':
+        return render(request, 'todo/create_todo.html')
+    else:
+        pass
+
+def current_todos(request):
+    return(render(request, 'todo/current_todos.html'))
 
